099aa04c1b73b16d2a2a0019e9d52e82f4ddc52f
SagarDev13 <sagardevkota293@gmail.com>
2025-09-16 17:44:54 +0545

    Update reverseString.js

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,5 +1,17 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+  let n = string.length; 
+  let reversed = "";
+  if (string.length = 0) {
+    console.log("Error!");
+  } else if (string.length = 0) {
+    return string;
+  }
+  else {
+  for (let i = 0; i < n; i++) {
+    reversed[i] = string[n - i - 1];
+  }
+  return reversed;
+  }
 };
 
 // Do not edit below this line
