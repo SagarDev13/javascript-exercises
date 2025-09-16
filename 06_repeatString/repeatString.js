99caf79840a8756c85301b7dee7c77b4cffc65b1
SagarDev13 <sagardevkota293@gmail.com>
2025-09-16 17:26:20 +0545

    Update repeatString.js

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,5 +1,10 @@
 const repeatString = function() {
-
+  repearString('hey', 3) // should return heyheyhey
+    let string = 'hey';
+    for (let i = 0; i < 3; i++) {
+      string += string;
+    }
+    return string;
 };
 
 // Do not edit below this line
