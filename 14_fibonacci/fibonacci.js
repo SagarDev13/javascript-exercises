d76ce4ba1025ce1ba1a6c2cf14756ec2f7970960
SagarDev13 <sagardevkota293@gmail.com>
2025-09-17 11:09:38 +0545

    Update fibonacci.js

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,5 +1,13 @@
-const fibonacci = function() {
-
+const fibonacci = function(n) {
+  let a = 0;
+  b = 1;
+  let number = 0;
+  for (let i = 1; i < n; i++) {
+    number = a + b;
+    a = b;
+    b = number; 
+  }
+  return number;
 };
 
 // Do not edit below this line
