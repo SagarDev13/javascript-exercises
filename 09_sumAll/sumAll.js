aab9b2aa057c09efa31231e630cf4fcc50e25b0c
SagarDev13 <sagardevkota293@gmail.com>
2025-09-17 09:06:51 +0545

    Update sumAll.js

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,6 +1,7 @@
 const sumAll = function(num1, num2) {
-  for (let i = 1; i <= num2 - num1; i++) {
-    num1 += 1;
+  for (let i = num1; i <= num2; i++) {
+    let sum = 0;
+    sum += i;
   }
   return num1;
 };
