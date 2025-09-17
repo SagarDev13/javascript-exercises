59a756cf0b046b8166a419eff7af035132f8cdfc
SagarDev13 <sagardevkota293@gmail.com>
2025-09-17 10:22:11 +0545

    Update palindromes.js

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,17 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+  let n = string.length;
+  let arr = string.split("");
+  let sum = 0;
+  for (int i = 0; i < n; i++) {
+    if (arr[i] != arr[n-1-i]) {
+      sum = 1;
+    }
+  }
+  if (sum == 0) {
+    return true;
+  } else if (sum == 1) {
+    return false;
+  }
 };
 
 // Do not edit below this line
