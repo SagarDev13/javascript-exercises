de5f337db1a95f4548383428fa8ac3ad3f2b1f79
SagarDev13 <sagardevkota293@gmail.com>
2025-09-17 10:06:36 +0545

    Update calculator.js

--- 12_calculator/calculator.js
+++ 12_calculator/calculator.js
@@ -1,25 +1,45 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+	return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	if (num1 < num2) {
+		let temp = num1;
+		num1 = num2;
+		num2 = temp;
+	}
+	return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	let n = arr.length;
+	let sum = 0;
+	for (int i = 0; i < n; i++) {
+		sum += arr[i];
+	}
+	return sum;
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+	let n = arr.length;
+	let mul = 0;
+	for (int i = 0; i < n; i++) {
+		mul *= arr[i];
+	}
+	return mul;
 };
 
-const power = function() {
-	
+const power = function(base, expo) {
+	return base**expo;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	let fact = 1;
+	for (let i = 1; i <= num; i++) {
+		fact *= i;
+	}
+	return fact;	
+	}	
 };
 
 // Do not edit below this line
