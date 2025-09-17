2ea20b12a0250856586bcf19eec0d37f121176b1
SagarDev13 <sagardevkota293@gmail.com>
2025-09-17 17:53:45 +0545

    Update findTheOldest.js

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -2,12 +2,12 @@ const findTheOldest = function(arr) {
   let birthDates = [];
   let l = arr.length;
   for (let i = 0; i < l; i++) {
-    birthDates[i] = arr.birthdate[i];
+    birthDates[i] = arr[i].dateOfBirth;
   }
   let oldest = birthDates[0];
   for (let i = 1; i < l; i++) {
     if (birthDates[i] >= oldest {
-      oldest = birthDates[i]
+      oldest = birthDates[i];
     }
   }
   return oldest;
